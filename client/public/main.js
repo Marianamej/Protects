7033df9bfeaa74613302298a5f09ceea64c002eb
Bryan Vera <brayanandresveragar@gmail.com>
2023-09-29 14:15:35 -0500

    Style : Diseño base de los articles para le banner

--- client/public/main.js
+++ client/public/main.js
@@ -10,6 +10,36 @@ closeMobileMenu.addEventListener('click', () => {
     menuMobile.classList.toggle('hide');
 });
 
-// if(window.innerWidth > 768) {
-//     menuMobile.classList.add('hide');
-// }
+
+// Get all the articles
+const articles = document.querySelectorAll('.hero-banner');
+const imagenesSlider = ["fondo-banner.png","fondo-banner-2.jpg", "fondo-banner-3.avif"]
+
+// Set the current article index to 0
+let currentArticleIndex = 0;
+
+// Set the interval to 5 seconds
+const ONE_SECOND_IN_MS = 1000
+const interval = ONE_SECOND_IN_MS * 5;
+
+// Create a function to change the current article
+function changeArticle() {
+  console.log("Hola");
+  // Hide the current article
+  articles[currentArticleIndex].classList.remove('active');
+
+  // Increment the current article index
+  currentArticleIndex++;
+
+  // If the current article index is greater than or equal to the number of articles, reset it to 0
+  if (currentArticleIndex >= articles.length) {
+    currentArticleIndex = 0;
+  }
+
+  // Show the new current article
+  articles[currentArticleIndex].style.background = `linear-gradient(0deg, rgba(0, 0, 0, 0.35) 0%, rgba(0, 0, 0, 0.35) 100%), url('../assets/${imagenesSlider[currentArticleIndex]}'), lightgray 50%;`
+  articles[currentArticleIndex].classList.add('active');
+}
+
+// Call the changeArticle function every 5 seconds
+setInterval(changeArticle, interval);
