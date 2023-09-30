08d5196206661728e79dfc7ca7738c1b5190af48
Bryan Vera <brayanandresveragar@gmail.com>
2023-09-29 20:53:24 -0500

    func : Creacion banner con transiciones y organizacion d ela simagenes

--- client/public/main.js
+++ client/public/main.js
@@ -1,3 +1,6 @@
+import { renderBanner } from '../renderBanner.js';
+import { getBanners,fondos } from '../apiBanner.js';
+
 const hamburguerMenu = document.getElementById('hamburguer-menu');
 const menuMobile = document.getElementById('menu-mobile');
 const closeMobileMenu = document.getElementById('mobile-menu-close');
@@ -11,9 +14,10 @@ closeMobileMenu.addEventListener('click', () => {
 });
 
 
+renderBanner(getBanners)
+
 // Get all the articles
-const articles = document.querySelectorAll('.hero-banner');
-const imagenesSlider = ["fondo-banner.png","fondo-banner-2.jpg", "fondo-banner-3.avif"]
+const banners = document.querySelectorAll('.hero-banner');
 
 // Set the current article index to 0
 let currentArticleIndex = 0;
@@ -26,19 +30,19 @@ const interval = ONE_SECOND_IN_MS * 5;
 function changeArticle() {
   console.log("Hola");
   // Hide the current article
-  articles[currentArticleIndex].classList.remove('active');
+  banners[currentArticleIndex].classList.remove('active');
 
   // Increment the current article index
   currentArticleIndex++;
 
-  // If the current article index is greater than or equal to the number of articles, reset it to 0
-  if (currentArticleIndex >= articles.length) {
+  // If the current article index is greater than or equal to the number of banners, reset it to 0
+  if (currentArticleIndex >= banners.length) {
     currentArticleIndex = 0;
   }
 
-  // Show the new current article
-  articles[currentArticleIndex].style.background = `linear-gradient(0deg, rgba(0, 0, 0, 0.35) 0%, rgba(0, 0, 0, 0.35) 100%), url('../assets/${imagenesSlider[currentArticleIndex]}'), lightgray 50%;`
-  articles[currentArticleIndex].classList.add('active');
+  // Show the next article
+  banners[currentArticleIndex].style = `background: linear-gradient(0deg, rgba(0, 0, 0, 0.35) 0%, rgba(0, 0, 0, 0.35) 100%), url(${fondos[currentArticleIndex]}) no-repeat center/cover, lightgray 50%;`;
+  banners[currentArticleIndex].classList.add('active');
 }
 
 // Call the changeArticle function every 5 seconds
