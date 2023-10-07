d5726bdd1c9bd388dcd53f5882e932ae5c4a3486
Bryan Vera <brayanandresveragar@gmail.com>
2023-10-07 12:41:45 -0500

    :art: Creacion de opciones

---
+++ client/scripts/products.js
@@ -0,0 +1,155 @@
+[
+    {
+      "imagen": "https://ejemplo.com/imagen1.jpg",
+      "nombre": "Portátil Premium",
+      "descripcion": "Un portátil de alto rendimiento para profesionales.",
+      "precio": 1299.99,
+      "descuento": true,
+      "numeroEstrellas": 4,
+      "precioDescuento": 1199.99,
+      "categoria": "Portatil"
+    },
+    {
+      "imagen": "https://ejemplo.com/imagen2.jpg",
+      "nombre": "PC de Escritorio Master",
+      "descripcion": "Potente PC de escritorio para tareas pesadas y juegos.",
+      "precio": 1799.99,
+      "descuento": false,
+      "numeroEstrellas": 5,
+      "categoria": "Desktop"
+    },
+    {
+      "imagen": "https://ejemplo.com/imagen3.jpg",
+      "nombre": "Monitor Curvo 4K",
+      "descripcion": "Experimenta imágenes nítidas y colores vibrantes con este monitor.",
+      "precio": 599.99,
+      "descuento": true,
+      "numeroEstrellas": 4,
+      "precioDescuento": 549.99,
+      "categoria": "Monitors"
+    },
+    {
+      "imagen": "https://ejemplo.com/imagen4.jpg",
+      "nombre": "CPU Cooler Avanzado",
+      "descripcion": "Mantén tu CPU a la temperatura perfecta con este refrigerador.",
+      "precio": 89.99,
+      "descuento": false,
+      "numeroEstrellas": 4,
+      "categoria": "Parts of pc"
+    },
+    {
+      "imagen": "https://ejemplo.com/imagen5.jpg",
+      "nombre": "Mouse Gamer Profesional",
+      "descripcion": "Un mouse diseñado para gamers con iluminación RGB.",
+      "precio": 49.99,
+      "descuento": true,
+      "numeroEstrellas": 5,
+      "precioDescuento": 39.99,
+      "categoria": "Complementos"
+    },
+    {
+      "imagen": "https://ejemplo.com/imagen6.jpg",
+      "nombre": "Portátil Ultraligero",
+      "descripcion": "El compañero perfecto para viajar y trabajar sobre la marcha.",
+      "precio": 999.99,
+      "descuento": false,
+      "numeroEstrellas": 4,
+      "categoria": "Portatil"
+    },
+    {
+      "imagen": "https://ejemplo.com/imagen7.jpg",
+      "nombre": "PC de Juegos Compacto",
+      "descripcion": "Un PC compacto pero potente para tus juegos favoritos.",
+      "precio": 1499.99,
+      "descuento": true,
+      "numeroEstrellas": 5,
+      "precioDescuento": 1399.99,
+      "categoria": "Desktop"
+    },
+    {
+      "imagen": "https://ejemplo.com/imagen8.jpg",
+      "nombre": "Monitor de Diseño",
+      "descripcion": "Un monitor de alta resolución para diseñadores y creativos.",
+      "precio": 699.99,
+      "descuento": false,
+      "numeroEstrellas": 4,
+      "categoria": "Monitors"
+    },
+    {
+      "imagen": "https://ejemplo.com/imagen9.jpg",
+      "nombre": "Tarjeta Gráfica Potente",
+      "descripcion": "Mejora el rendimiento de tu PC con esta tarjeta gráfica de última generación.",
+      "precio": 349.99,
+      "descuento": true,
+      "numeroEstrellas": 4,
+      "precioDescuento": 329.99,
+      "categoria": "Parts of pc"
+    },
+    {
+      "imagen": "https://ejemplo.com/imagen10.jpg",
+      "nombre": "Teclado Mecánico RGB",
+      "descripcion": "Un teclado de alta calidad con retroiluminación personalizable.",
+      "precio": 79.99,
+      "descuento": false,
+      "numeroEstrellas": 5,
+      "categoria": "Complementos"
+    },
+    {
+      "imagen": "https://ejemplo.com/imagen11.jpg",
+      "nombre": "Portátil Elegante",
+      "descripcion": "Un portátil elegante y ligero para uso diario.",
+      "precio": 1099.99,
+      "descuento": true,
+      "numeroEstrellas": 4,
+      "precioDescuento": 999.99,
+      "categoria": "Portatil"
+    },
+    {
+      "imagen": "https://ejemplo.com/imagen12.jpg",
+      "nombre": "PC de Oficina Compacto",
+      "descripcion": "Una PC compacta ideal para tareas de oficina.",
+      "precio": 899.99,
+      "descuento": false,
+      "numeroEstrellas": 4,
+      "categoria": "Desktop"
+    },
+    {
+      "imagen": "https://ejemplo.com/imagen13.jpg",
+      "nombre": "Monitor Curvo Gaming",
+      "descripcion": "Un monitor curvo diseñado para una experiencia de juego inmersiva.",
+      "precio": 799.99,
+      "descuento": true,
+      "numeroEstrellas": 5,
+      "precioDescuento": 749.99,
+      "categoria": "Monitors"
+    },
+    {
+      "imagen": "https://ejemplo.com/imagen14.jpg",
+      "nombre": "Unidad de Estado Sólido (SSD) de Alta Velocidad",
+      "descripcion": "Acelera tu PC con esta unidad de estado sólido ultrarrápida.",
+      "precio": 129.99,
+      "descuento": false,
+      "numeroEstrellas": 4,
+      "categoria": "Parts of pc"
+    },
+    {
+      "imagen": "https://ejemplo.com/imagen15.jpg",
+      "nombre": "Mouse Inalámbrico Ergonómico",
+      "descripcion": "Un mouse ergonómico diseñado para la comodidad durante todo el día.",
+      "precio": 39.99,
+      "descuento": true,
+      "numeroEstrellas": 5,
+      "precioDescuento": 34.99,
+      "categoria": "Complementos"
+    },
+    {
+      "imagen": "https://ejemplo.com/imagen16.jpg",
+      "nombre": "Portátil 2 en 1",
+      "descripcion": "Un portátil versátil que también se convierte en una tablet.",
+      "precio": 899.99,
+      "descuento": true,
+      "numeroEstrellas": 4,
+      "precioDescuento": 849.99,
+      "categoria": "Portatil"
+    }
+]
