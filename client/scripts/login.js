a124abd4a98c8027ab9214e5c9ad9d0ecdd73f80
Bryan Vera <brayanandresveragar@gmail.com>
2023-10-16 21:20:11 -0500

    :bug: Se arreglo bug en la seccion Login

--- client/scripts/login.js
+++ client/scripts/login.js
@@ -1,15 +1,21 @@
-const loginForm = document.querySelector('#loginForm')
-loginForm.addEventListener('submit', (e)=>{
+
+const loginButton = document.querySelector('#login__button')
+
+loginButton.addEventListener('click', (e)=>{
     e.preventDefault()
+    
     const email = document.querySelector('#email').value
     const password = document.querySelector('#password').value
-    const Users = JSON.parse(localStorage.getItem('users')) || []
-    const validUser = Users.find(user => user.email === email && user.password === password)
+
+    const users = JSON.parse(localStorage.getItem('users')) || []
+
+    const validUser = users.find(user => user.email === email && user.password === password)
+
     if(!validUser){
         return alert('Usuario y/o contraseña incorrectos!')
     }
+
     alert(`Bienvenido ${validUser.name}`)
     localStorage.setItem('login_success', JSON.stringify(validUser))
-    window.location.href = 'index.html'   
-//NO SIRVE TOCA EDITARLO CON LA INFORMACION DE LA BASE DE DATOS
+    window.location.href = 'home.html'   
 })
