cec5ebad9b8f162578c13b652f90a44722661bdc
Bryan Vera <brayanandresveragar@gmail.com>
2023-11-02 13:35:04 -0500

    Variables de entorno

--- server/src/main/java/com/gamertx/GamertxShopApplication.java
+++ server/src/main/java/com/gamertx/GamertxShopApplication.java
@@ -6,8 +6,10 @@ import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-secrets.properties")
 public class GamertxShopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GamertxShopApplication.class, args);
