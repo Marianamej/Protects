6dd03a44165f385ff62e0c3aa389be32a80c250a
Bryan Vera <brayanandresveragar@gmail.com>
2023-11-07 14:35:49 -0500

    :sparkles: End points de info usuario y actualizar usuario

--- server/src/main/java/com/gamertx/persistence/entity/products_view/Pedido.java
+++ server/src/main/java/com/gamertx/persistence/entity/products_view/Pedido.java
@@ -1,6 +1,8 @@
 package com.gamertx.persistence.entity.products_view;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.gamertx.persistence.entity.EnumContent;
+import com.gamertx.persistence.entity.users_view.Usuario;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,6 +12,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -45,4 +49,5 @@ public class Pedido {
 
     @OneToMany(mappedBy = "pedido", cascade = {CascadeType.ALL}, fetch= FetchType.EAGER)
     List<DetallePedido> productos;
+
 }
