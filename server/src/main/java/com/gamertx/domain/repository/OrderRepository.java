cb5696403d1f357fd931828b6f877203bd47d6e2
Bryan Vera <brayanandresveragar@gmail.com>
2023-11-08 15:50:27 -0500

    :sparkles: Actualizacion en el end point de carrito

--- server/src/main/java/com/gamertx/domain/repository/OrderRepository.java
+++ server/src/main/java/com/gamertx/domain/repository/OrderRepository.java
@@ -1,6 +1,9 @@
 package com.gamertx.domain.repository;
 
+import com.gamertx.domain.Item;
 import com.gamertx.domain.Order;
+import com.gamertx.domain.Product;
+import com.gamertx.persistence.entity.products_view.Pedido;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,4 +12,6 @@ public interface OrderRepository {
     List<Order> getAll();
     Optional<List<Order>> getByClient(String email);
     Order save(Order order);
+    List<Product> getItemsByClient(String email);
+    Order addItem(Item item, Integer orderId);
 }
